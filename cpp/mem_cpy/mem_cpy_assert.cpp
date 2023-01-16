be4251a5dee77a8bda0f9067e85e8c360dcfa06d
Tapan Pandey <tapan@programmingdays.com>
2023-01-16 17:11:48 +0530

    Updated assertions test

--- cpp/mem_cpy/mem_cpy_assert.cpp
+++ cpp/mem_cpy/mem_cpy_assert.cpp
@@ -9,6 +9,10 @@
 void test_case_one_char_len()
 {
     const char * p_test_name = "Test with one character length";
+    
+    printf("\033[0;32m"); 
+    printf("\nTest case: %s\n", p_test_name);
+    printf("\033[0;0m");
 
     const uint8_t k_MAXLEN = 1;
     uint8_t u8_dest_buf[k_MAXLEN] = {'#'};
@@ -24,6 +28,10 @@ void test_case_one_char_len()
 void test_case_with_offset()
 {
     const char * p_test_name = "Test with offset";
+    printf("\033[0;32m"); 
+    printf("\nTest case: %s\n", p_test_name);
+    printf("\033[0;0m"); 
+    
     const uint8_t k_MAXLEN = 10;
     uint8_t u8_dest_buf[] = "##########";
     const char * u8_src_buf  = "Amigos";
@@ -45,6 +53,11 @@ void test_case_with_offset()
 void test_case_buffer_over()
 {
     const char *p_test_name = "Test with length more than buffer";
+    
+    printf("\033[0;32m"); 
+    printf("\nTest case: %s\n", p_test_name);
+    printf("\033[0;0m");
+
     const uint8_t k_MAXLEN = 10;
     uint8_t u8_dest_buf[] = "##########";
     const char * u8_src_buf  = "La casa de papel"; // 15 characters
@@ -62,49 +75,63 @@ void test_case_buffer_over()
 }
 
 
-// TEST_CASE("Test with length more than buffer and offset", "[mem_cpy_one_plus_len]")
-// {
-//     const uint8_t k_MAXLEN = 10;
-//     uint8_t u8_dest_buf[] = "##########";
-//     const char * u8_src_buf  = "La casa de papel"; // 15 characters
+void test_case_buffer_over_offset()
+{
+    const char *p_test_name = "Test with length more than buffer and offset";
+    printf("\033[0;32m"); 
+    printf("\nTest case: %s\n", p_test_name);
+    printf("\033[0;0m");
 
-//     memcpy(u8_dest_buf + 4, u8_src_buf, safe_min(sizeof(u8_dest_buf) - 4, strlen(u8_src_buf)));
+    const uint8_t k_MAXLEN = 10;
+    uint8_t u8_dest_buf[] = "##########";
+    const char * u8_src_buf  = "La casa de papel"; // 15 characters
+
+    memcpy(u8_dest_buf + 4, u8_src_buf, safe_min(sizeof(u8_dest_buf) - 4, strlen(u8_src_buf)));
     
-//     std::for_each(u8_dest_buf, u8_dest_buf + k_MAXLEN, [](uint8_t &c) { std::cout << '[' << c << ']';});
-//     std::cout << std::endl;
-//     assert(u8_dest_buf[0] == '#');
-//     assert(u8_dest_buf[3] == '#');
+    std::for_each(u8_dest_buf, u8_dest_buf + k_MAXLEN, [](uint8_t &c) { std::cout << '[' << c << ']';});
+    std::cout << std::endl;
+    assert(u8_dest_buf[0] == '#');
+    assert(u8_dest_buf[3] == '#');
 
-//     assert(u8_dest_buf[4] == 'L');
-//     assert(u8_dest_buf[k_MAXLEN - 1] == 's');
+    assert(u8_dest_buf[4] == 'L');
+    assert(u8_dest_buf[k_MAXLEN - 1] == 's');
 
-//     std::cout << std::endl;
-// }
+    std::cout << std::endl;
+}
 
 
-// TEST_CASE("Test with length more than buffer and offset in source", "[mem_cpy_one_plus_len]")
-// {
-//     const uint8_t k_MAXLEN = 10;
-//     uint8_t u8_dest_buf[] = "##########";
-//     const char * u8_src_buf  = "La casa de papel"; // 15 characters
+void test_case_buffer_over_offset_source()
+{
+    const char *p_test_name = "Test with length more than buffer and offset in source";
+    printf("\033[0;32m"); 
+    printf("\nTest case: %s\n", p_test_name);
+    printf("\033[0;0m");
 
-//     memcpy(u8_dest_buf + 2, u8_src_buf + 10, safe_min(sizeof(u8_dest_buf) - 2, strlen(u8_src_buf) - 10));
+    const uint8_t k_MAXLEN = 10;
+    uint8_t u8_dest_buf[] = "##########";
+    const char * u8_src_buf  = "La casa de papel"; // 15 characters
+
+    memcpy(u8_dest_buf + 2, u8_src_buf + 10, safe_min(sizeof(u8_dest_buf) - 2, strlen(u8_src_buf) - 10));
     
-//     std::for_each(u8_dest_buf, u8_dest_buf + k_MAXLEN, [](uint8_t &c) { std::cout << '[' << c << ']';});
-//     std::cout << std::endl;
-//     assert(u8_dest_buf[0] == '#');
-//     assert(u8_dest_buf[3] == 'p');
+    std::for_each(u8_dest_buf, u8_dest_buf + k_MAXLEN, [](uint8_t &c) { std::cout << '[' << c << ']';});
+    std::cout << std::endl;
+    assert(u8_dest_buf[0] == '#');
+    assert(u8_dest_buf[3] == 'p');
 
-//     assert(u8_dest_buf[k_MAXLEN - 1] == '#');
+    assert(u8_dest_buf[k_MAXLEN - 1] == '#');
 
-//     std::cout << std::endl;
-// }
+    std::cout << std::endl;
+}
+
+
 int main(int argc, char **argv)
 {
 
     test_case_one_char_len();
     test_case_with_offset();
     test_case_buffer_over();
+    test_case_buffer_over_offset();
+    test_case_buffer_over_offset_source();
 
     return EXIT_SUCCESS;
 }
